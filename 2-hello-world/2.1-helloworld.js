5fdd160f003f5638c95fe2d78810c28abb1ac136
lsmucassi <lindasmucassi@gmail.com>
2020-12-22 14:28:28 +0200

    2: Javascript Hello world

--- 2-hello-world/2.1-helloworld.js
+++ 2-hello-world/2.1-helloworld.js
@@ -1,2 +1,6 @@
-console.log('Referenced javascript from a .js file')
-console.log('Hello, World!');
+console.log('From a .js file:[File] Hello, World!')
+console.log('');
+document.getElementsByTagName("h1")[0].style.fontSize = "50px";
+document.getElementsByTagName("h1")[0].style.textAlign = "center";
+document.getElementsByTagName("h1")[0].style.color = "white";
+document.body.style.backgroundColor = "grey";
